61558fea95b8b3f73d7a9642f2878c20c02adbf0
AlexanderChibirev <www.darmagh@mail.ru>
2015-12-20 15:32:42 +0300

    пробник

--- lab4-5/сonfig.h
+++ lab4-5/сonfig.h
@@ -2,37 +2,36 @@
 using namespace sf;
 using namespace std;
 
+
 struct Sys {
 	Vector2u window_size = Vector2u(800, 600);
 	string dir_path = "";
 }typedef Sys;
 
 struct Files {
-	string *files = new string[0];
-	unsigned int name_size = 0;
-	unsigned int arr_size = 0;
+	int name_size = 0;
+	int arr_size = 0;
+
+	string *files = nullptr;
 	string path;
+
 }typedef Files;
 
 struct Picture {
-	float shift_x;
-	float shift_y;
-	float st_drag_x, st_drag_y;
-	bool dragMove = false;
+	int num = 0;
 	float zoom = 1.0;
-	float smoth_zoom = 0.25;
-	string title = "Error";
 	float left;
 	float top;
-	Texture *texture = new Texture;
-	Texture *p_texture = new Texture;
-	Texture *n_texture = new Texture;
-	Sprite *sprite = new Sprite;
-	bool pr = false;
-	bool ne = false;
+	float shift_x;
+	float shift_y;
+	float st_drag_x, st_drag_y;	
+	float smoth_zoom = 0.25;
+	bool drag_move = false;
 	bool error = true;
-	unsigned int num = 0;
-	float size;
+	string title = "Error";
+	Texture *texture = nullptr;
+	Sprite *sprite = nullptr;
+
 }typedef Picture;
 
 
