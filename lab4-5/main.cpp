9bd0305b73c4d30ebf2f7ad35c8673b21cdf9cee
AlexanderChibirev <www.darmagh@mail.ru>
2015-12-31 01:41:31 +0300

    исп

--- lab4-5/main.cpp
+++ lab4-5/main.cpp
@@ -1,53 +1,83 @@
-#include <SFML/Graphics.hpp>
-#include "config.h"
+
 #include <string>
 #include <array>
 #include <iostream>
 #include <algorithm>
 #include <windows.h>
+#include <clocale>
+#include <locale>
+#include <tchar.h>
+#include <cwchar>
+#include <vector>
+#include <stdio.h>
+#include "config.h"
 
 
-//в файлах которых нет точки не надо обрабатывать
 
-bool is_image(string file_name) {
-	string fileExt = file_name.substr(file_name.find_last_of(".") + 1);
-	std::array<string, 5> extArray = { "jpg","jpeg","png","gif","bmp" };
-	if (fileExt.size() == 0) {
+
+bool is_image(string file_name, string path) { 
+	
+	if (!strrchr(file_name.c_str(), '.')) {
 		return false;
 	}
-	return std::any_of(extArray.begin(), extArray.end(), [&](const std::string &ext) {
-		return ext == fileExt;
-	});
+	std::array<string, 5> extArray = { "jpg","jpeg","png","gif","bmp" };
+	if(std::any_of(extArray.begin(), extArray.end(), [&](const std::string &ext) {
+		return ext == file_name.substr(file_name.find_last_of(".") + 1);
+	})){
+	
+		Image image;
+		if (!(image.loadFromFile(path + file_name))) {
+			return false;
+		}
+		else {
+			return true;
+		}
+
+	}
+	return true;
 }
 
 bool directory_exists(string &fileName) { 
-	int code = GetFileAttributes(fileName.c_str());
+	int code = GetFileAttributes((LPCWSTR)fileName.c_str());
 	return (code != -1) && (FILE_ATTRIBUTE_DIRECTORY & code);
+	//return true;
 }
 
-Files get_file_list(string const & oldPath) {//убрать поле из path убрать структуру files, показывать ошибки если файл txt 
+string narrow(std::wstring const& text)
+{
+	locale const loc("");
+	wchar_t const* from = text.c_str();
+	size_t const len = text.size();
+	vector<char> buffer(len + 1);
+	use_facet<std::ctype<wchar_t> >(loc).narrow(from, from + len, '_', &buffer[0]);
+	return string(&buffer[0], &buffer[len]);
+}
+
+
+vector<string> get_file_list(string const & oldPath) {
 	string path = oldPath + string("*");
-	Files files;
-	files.path = oldPath;
+	vector<string> files;
+	
 	WIN32_FIND_DATA fileData;
-	HANDLE firstFile = FindFirstFile(path.c_str(), &fileData);
+	HANDLE firstFile = FindFirstFile((LPCWSTR)path.c_str(), &fileData);//поиск  файла
 	if (firstFile != INVALID_HANDLE_VALUE) {
-		firstFile = FindFirstFile(path.c_str(), &fileData);
 		do {
 			if (!(fileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-				if (is_image(fileData.cFileName)) {
-					files.files.push_back(fileData.cFileName);				
+				if (is_image(narrow(fileData.cFileName),oldPath)) {
+					files.push_back(narrow(fileData.cFileName));
 				}
 			}
 
 		} while (FindNextFile(firstFile, &fileData));
+		
 		FindClose(firstFile);
 	}
+	
 	return files;
 }
 
 
-void sprite_for_window(Picture &pict, Sys &sys, Files const &files) {
+void sprite_for_window(Picture &pict, Sys &sys, vector<string> const &files) {
 	pict.sprite = new Sprite;
 	pict.sprite->setPosition(0, 0);
 	pict.sprite->setTexture(*(pict.texture));
@@ -57,19 +87,19 @@ void sprite_for_window(Picture &pict, Sys &sys, Files const &files) {
 	}
 	pict.sprite->setScale(pict.zoom, pict.zoom);
 	pict.sprite->setOrigin(float(pict.texture->getSize().x * 0.5), float(pict.texture->getSize().y / 2.0));
-	pict.title = string(files.files[pict.num]);
+	pict.title = string(files[pict.num]);
 }
 
-string init_picture(RenderWindow & window, Files const &files, Picture &pic, Sys & sys) {
+string init_picture(RenderWindow & window, vector<string> const &files, string const &path, Picture &pic, Sys & sys) {
 	if (!(pic.num < 0)) {
 		if (!pic.error) {
 			delete(pic.sprite);
 		}
 		Image *image = new Image;
-		string path = files.path + files.files[pic.num];
+		string path = path + files[pic.num];
 
-		if (!(image->loadFromFile(files.path + files.files[pic.num]))) {
-			std::cout << "error with: \n" << files.path + files.files[pic.num] << '\n';
+		if (!(image->loadFromFile(path + files[pic.num]))) {
+			std::cout << "error with: \n" << path + files[pic.num] << '\n';
 			pic.error = true;
 		}
 		else {
@@ -97,26 +127,26 @@ void picture_mid(Vector2u window_size, Picture &picture) {
 	picture.sprite->setPosition(Vector2f(picture.left_bias, picture.top));
 }
 
-void switch_prev(RenderWindow & window, Picture & picture, Files & files, Sys & sys) {
+void switch_prev(RenderWindow & window, Picture & picture, vector<string> & files, string const &path, Sys & sys) {
 	picture.zoom = 1;
 	picture.smoth_zoom = 0.25;
 	if (picture.num == 0) {
-		picture.num = files.files.size();
+		picture.num = files.size();
 	}
 	picture.num--;
-	window.setTitle(init_picture(window, files, picture, sys));
+	window.setTitle(init_picture(window, files, path, picture, sys));
 }
 
-void switch_next(RenderWindow & window, Picture &picture, Files & files, Sys & sys) {
+void switch_next(RenderWindow & window, Picture &picture, vector<string> & files, string const &path, Sys & sys) {
 	picture.zoom = 1;
 	picture.smoth_zoom = 0.25;
-	if (picture.num + 1 == files.files.size()) {
+	if (picture.num + 1 == files.size()) {
 		picture.num = 0;
 	}
 	else {
 		picture.num++;
 	}
-	window.setTitle(init_picture(window, files, picture, sys));
+	window.setTitle(init_picture(window, files, path, picture, sys));
 }
 
 
@@ -149,40 +179,40 @@ void draw_elements(RenderWindow & window, Sys & sys, Picture & picture) {
 }
 
 
-void click_btn_mouse(RenderWindow &window, Files files, Picture &picture, Sys sys, Vector2f pos) {
+void click_btn_mouse(RenderWindow &window, vector<string> files, string const &path,Picture &picture, Sys sys, Vector2f pos) {
 	if (!picture.drag_move) {
 		if (picture.left.getGlobalBounds().contains(pos.x, pos.y)) {
-			switch_prev(window, picture, files, sys);
+			switch_prev(window, picture, files, path, sys);
 		}
 		else if (picture.right.getGlobalBounds().contains(pos.x, pos.y)) {
-			switch_next(window, picture, files, sys);
+			switch_next(window, picture, files, path, sys);
 		}
 		else if (picture.plus.getGlobalBounds().contains(pos.x, pos.y) && picture.zoom < 5) {
 			picture.zoom += picture.smoth_zoom;
-			window.setTitle(init_picture(window, files, picture, sys));
+			window.setTitle(init_picture(window, files, path, picture, sys));
 		}
 		else if (picture.minus.getGlobalBounds().contains(pos.x, pos.y) && picture.zoom > picture.smoth_zoom) {
 			picture.zoom -= picture.smoth_zoom;
-			window.setTitle(init_picture(window, files, picture, sys));
+			window.setTitle(init_picture(window, files, path, picture, sys));
 		}
 	}
 }
 
-void click_btn(RenderWindow &window, Files files, Picture &picture, Sys sys, Vector2f pos) {
+void click_btn(RenderWindow &window, vector<string> files, string const &path, Picture &picture, Sys sys, Vector2f pos) {
 	if (!picture.drag_move) {
 		if (Keyboard::isKeyPressed(Keyboard::Left)) {
-			switch_prev(window, picture, files, sys);
+			switch_prev(window, picture, files, path, sys);
 		}
 		else if (Keyboard::isKeyPressed(Keyboard::Right)) {
-			switch_next(window, picture, files, sys);
+			switch_next(window, picture, files, path, sys);
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Add) && Keyboard::isKeyPressed(Keyboard::LControl) && picture.zoom < 5) {
 			picture.zoom += picture.smoth_zoom;
-			window.setTitle(init_picture(window, files, picture, sys));
+			window.setTitle(init_picture(window, files, path, picture, sys));
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Subtract) && Keyboard::isKeyPressed(Keyboard::LControl) && picture.zoom > picture.smoth_zoom) {
 			picture.zoom -= picture.smoth_zoom;
-			window.setTitle(init_picture(window, files, picture, sys));
+			window.setTitle(init_picture(window, files, path, picture, sys));
 		}
 	}
 }
@@ -202,21 +232,21 @@ void picture_check_drag(Picture &picture, Vector2f pos, Sys sys) {
 		picture.shift_xy.y = picture_pos.y - picture.st_drag_xy.y;
 	}
 }
-void start_program(RenderWindow &window, Files files, Picture &picture, Sys sys, View view) {
+void start_program(RenderWindow &window, vector<string> files, string const &path, Picture &picture, Sys sys, View view) {
 	while (window.isOpen()) {
 		Event event;
 		Vector2i pixelPos = Mouse::getPosition(window);
 		Vector2f pos = window.mapPixelToCoords(pixelPos);
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed) {
-				files.files.clear(); // очистка памяти
+				files.clear(); // очистка памяти
 				window.close();
 			}
 			if (picture.drag_move) {
 				drag(picture, pos, sys);
 			}
 			if (event.type == Event::MouseButtonPressed && event.key.code == Mouse::Left) {
-				click_btn_mouse(window, files, picture, sys, pos);
+				click_btn_mouse(window, files, path, picture, sys, pos);
 				picture_check_drag(picture, pos, sys);
 			}
 
@@ -224,7 +254,7 @@ void start_program(RenderWindow &window, Files files, Picture &picture, Sys sys,
 				picture.drag_move = false;
 			}
 
-			click_btn(window, files, picture, sys, pos);
+			click_btn(window, files, path, picture, sys, pos);
 
 			if (event.type == Event::Resized) {
 				sys.window_size = window.getSize();
@@ -248,24 +278,27 @@ int main() {
 	Picture picture;
 	cout << "input path " << "\n";
 	//
+	string path;
 	while (!directory_exists(sys.dir_path)) {
 		sys.dir_path = "C:\\images\\";
+		path = sys.dir_path;
+		//cin >> sys.dir_path;
 	}
 	//
 	RenderWindow window(VideoMode(sys.window_size.x, sys.window_size.y), "Image Viewer");
 	window.setVerticalSyncEnabled(true);
 	View view = View(FloatRect(0, 0, float(sys.window_size.x), float(sys.window_size.y)));
 	window.setView(view);
-	Files files = get_file_list(sys.dir_path);
+	vector<string> files = get_file_list(sys.dir_path);
 
-	if (files.files.size() > 0) {
-		window.setTitle(init_picture(window, files, picture, sys));
+	if (files.size() > 0) {
+		window.setTitle(init_picture(window, files, path, picture, sys));
 	}
 	picture.is_initializing_toolbar(picture);
 	sys.window_size = window.getSize();
 	picture_mid(sys.window_size, picture);
 
-	start_program(window, files, picture, sys, view);
+	start_program(window, files, path, picture, sys, view);
 
 	return 0;
 }
